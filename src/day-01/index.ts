f23b582f3ef4278762d57c39fe63f28f4400e975
JanMouwes <jan.mouwes@gmail.com>
2021-12-01 11:31:20 +0100

    Improved code structure

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -3,13 +3,7 @@ export function part1(fileContents: string) {
   const numbers: number[] = fileContents.split("\n").map(n => Number(n));
 
   const increases = numbers.filter((num, index, arr) => {
-    if (index ===0) return false;
-
-    const prev = arr[index-1]
-console.log(prev, num, prev < num);
-
-
-    return arr[index-1] < num;
+    return index !== 0 && arr[index-1] < num;
   })
 
   return increases.length;
@@ -19,10 +13,10 @@ export function part2(fileContents: string) {
   const numbers: number[] = fileContents.split("\n").map(n => Number(n));
 
   const windows = numbers.map((num, index, arr) => {
-    if (index < 2) return 0;
+    if (index < 2) { return 0; }
 
     return arr[index-2] + arr[index-1] + num;
-  }).splice(2)
+  }).splice(2);
 
   return part1(windows.join("\n"));
 }
