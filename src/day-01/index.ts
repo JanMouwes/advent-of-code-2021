81f04d250032c6be7a6b7e031a76c61ae363af19
JanMouwes <jan.mouwes@gmail.com>
2021-12-01 08:39:56 +0100

    Day 1, part 1 and 2

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,8 +1,28 @@
 
 export function part1(fileContents: string) {
-  return fileContents;
+  const numbers: number[] = fileContents.split("\n").map(n => Number(n));
+
+  const increases = numbers.filter((num, index, arr) => {
+    if (index ===0) return false;
+
+    const prev = arr[index-1]
+console.log(prev, num, prev < num);
+
+
+    return arr[index-1] < num;
+  })
+
+  return increases.length;
 }
 
 export function part2(fileContents: string) {
-  return fileContents;
+  const numbers: number[] = fileContents.split("\n").map(n => Number(n));
+
+  const windows = numbers.map((num, index, arr) => {
+    if (index < 2) return 0;
+
+    return arr[index-2] + arr[index-1] + num;
+  }).splice(2)
+
+  return part1(windows.join("\n"));
 }
