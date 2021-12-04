4007d194de27480f4b42ae5602da7845cbf2372f
JanMouwes <jan.mouwes@gmail.com>
2021-12-04 11:11:40 +0100

    Added template

---
+++ src/day-template/index.ts
@@ -0,0 +1,16 @@
+export function part1(fileContents: string) {
+  const parsed: any = parseInput(fileContents);
+
+  return parsed;
+}
+
+export function part2(fileContents: string) {
+  const parsed: any = parseInput(fileContents);
+
+  return parsed;
+}
+
+
+function parseInput(input: string): any {
+  return input;
+}
