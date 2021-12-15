d7d84479ebedf4a7cf9e83aa3b8deea753f3efd3
JanMouwes <jan.mouwes@gmail.com>
2021-12-16 00:21:54 +0100

    Refactored day 12

--- src/day-12/index.ts
+++ src/day-12/index.ts
@@ -35,65 +35,16 @@ export function part1(fileContents: string) {
   const allCaves = uniques(
     connections.map(first).concat(connections.map(second))
   );
-  const [smallCaves] = filterInOut(
-    allCaves,
-    (cave) => cave.toLowerCase() === cave
-  );
+  const smallCaves = new Set(first(filterInOut(allCaves, (cave) => cave.toLowerCase() === cave)));
 
   const graph = new Map(
     [...partition(connections, first).entries()].map(([start, paths]) => [
       start,
-      new Set(paths.map(second)),
+      uniques(paths.map(second)),
     ])
   );
 
-  function findPaths(path: string[], graph: Graph<string>): string[][] {
-    const [head, ...currentPath] = path;
-
-    if (head === "end") {
-      return [path];
-    }
-
-    const edges = graph.get(head) || [];
-
-    return [...edges]
-      .map((edge) => {
-        if (smallCaves.includes(edge) && currentPath?.includes(edge)) {
-          return [];
-        }
-
-        return findPaths([edge, head, ...currentPath], graph);
-      })
-      .flat();
-  }
-
-  const paths = findPaths(["start"], graph);
-
-  return paths.length;
-}
-
-type Graph<T> = Map<T, Set<T>>;
-
-export function part2(fileContents: string) {
-  const parsed = parseInput(fileContents);
-  const connections = parsed.concat(parsed.map(([a, b]) => [b, a]));
-
-  const allCaves = uniques(
-    connections.map(first).concat(connections.map(second))
-  );
-  const [smallCaves] = filterInOut(
-    allCaves,
-    (cave) => cave.toLowerCase() === cave
-  );
-
-  const graph = new Map(
-    [...partition(connections, first).entries()].map(([start, paths]) => [
-      start,
-      new Set(paths.map(second)),
-    ])
-  );
-
-  function findPaths(path: string[], graph: Graph<string>): string[][] {
+  function findPaths(path: string[]): string[][] {
     const [head] = path;
 
     if (head === "end") {
@@ -102,35 +53,64 @@ export function part2(fileContents: string) {
 
     const edges = graph.get(head) || [];
 
-    return [...edges]
-      .map((edge) => {
-        const isSecondVisit = path.includes(edge);
-        if (["start", "end"].includes(edge) && isSecondVisit) {
-          return [];
-        }
-
-        function countSmallCaves() {
-          const cnts = count(
-            path.filter((x) => smallCaves.includes(x)),
-            id
-          );
-          return Math.max(...cnts.values());
-        }
-
-        if (
-          smallCaves.includes(edge) &&
-          isSecondVisit &&
-          countSmallCaves() > 1
-        ) {
-          return [];
-        }
-
-        return findPaths([edge, ...path], graph);
-      })
+    return edges
+      .filter(edge => !smallCaves.has(edge) || !path.includes(edge))
+      .map((edge) => findPaths([edge].concat(path)))
       .flat();
   }
 
-  const paths = findPaths(["start"], graph);
+  const paths = findPaths(["start"]);
+
+  return paths.length;
+}
+
+export function part2(fileContents: string) {
+  const parsed = parseInput(fileContents);
+  const connections = parsed.concat(parsed.map(([a, b]) => [b, a]));
+
+  const allCaves = uniques(
+    connections.map(first).concat(connections.map(second))
+  );
+  const smallCaves = new Set(first(filterInOut(allCaves, (cave) => cave.toLowerCase() === cave)));
+
+  const graph = new Map(
+    [...partition(connections, first).entries()].map(([start, paths]) => [
+      start,
+      uniques(paths.map(second)),
+    ])
+  );
+
+  function findPaths(path: string[]): string[][] {
+    const [head] = path;
+
+    if (head === "end") {
+      return [path];
+    }
+
+    const edges = graph.get(head) || [];
+
+    return edges
+      .filter(edge => {
+        if (["start", "end"].includes(edge) && path.includes(edge)) {
+          return false;
+        }
+
+        function countSmallCaves(): number {
+          const cnts = count(path.filter((x) => smallCaves.has(x)), id).values();
+          return Math.max(...cnts);
+        }
+
+        return !(
+          smallCaves.has(edge) &&
+          path.includes(edge) &&
+          countSmallCaves() > 1
+        )
+      })
+      .map((edge) => findPaths([edge].concat(path)))
+      .flat();
+  }
+
+  const paths = findPaths(["start"]);
 
   return paths.length;
 }
