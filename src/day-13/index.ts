6441b5213cad47f9d4defbf19a67c0a99563df0f
JanMouwes <jan.mouwes@gmail.com>
2021-12-16 12:57:21 +0100

    Ran formatter

--- src/day-13/index.ts
+++ src/day-13/index.ts
@@ -1,8 +1,8 @@
-import { Tuple, second, equals } from '../util/tuple';
-import { lines } from '../util/string';
-import { Coord } from '../util/grid';
-import { first, filterInOut, uniques, head } from '../util/list';
-import { range } from '../util/range';
+import { Tuple, second, equals } from "../util/tuple";
+import { lines } from "../util/string";
+import { Coord } from "../util/grid";
+import { first, filterInOut, uniques, head } from "../util/list";
+import { range } from "../util/range";
 
 export const examples = {
   input: `6,10
@@ -37,29 +37,38 @@ export function part1(fileContents: string) {
 
   const folded = fold(dots, first(instructions));
 
-  return folded.length
+  return folded.length;
 }
 
 function fold(dots: Coord[], [axis, value]: Instruction): Coord[] {
   const index = axis === "x" ? 0 : 1;
 
   function moveDot(dot: Coord) {
-    const copy: [number, number] = [...dot]
+    const copy: [number, number] = [...dot];
 
-    copy[index] = dot[index] - (2 * (dot[index] - value));
+    copy[index] = dot[index] - 2 * (dot[index] - value);
 
     return copy as Coord;
   }
 
-  const [firstHalf, secondHalf] = filterInOut(dots, dot => dot[index] < value);
+  const [firstHalf, secondHalf] = filterInOut(
+    dots,
+    (dot) => dot[index] < value
+  );
 
   return uniques(firstHalf.concat(secondHalf.map(moveDot)));
 }
 
 function toString(dots: Coord[], [width, height]: readonly [number, number]) {
-  const contains = (target: Coord) => dots.find(dot => equals(dot, target))
+  const contains = (target: Coord) => dots.find((dot) => equals(dot, target));
 
-  return range(height).map(y => range(width).map(x => contains([x, y]) ? "#" : ".").join("")).join("\n")
+  return range(height)
+    .map((y) =>
+      range(width)
+        .map((x) => (contains([x, y]) ? "#" : "."))
+        .join("")
+    )
+    .join("\n");
 }
 
 export function part2(fileContents: string) {
@@ -68,28 +77,28 @@ export function part2(fileContents: string) {
   const result = instructions.reduce(fold, dots);
 
   const [xs, ys] = filterInOut(instructions, ([ax]) => ax === "x");
-  const size: [number, number] = ([
+  const size: [number, number] = [
     Math.min(...xs.map(second)),
-    Math.min(...ys.map(second))
-  ]);
+    Math.min(...ys.map(second)),
+  ];
 
   return toString(result, size);
 }
 
 type Axis = "x" | "y";
-type Instruction = Tuple<Axis, number>
+type Instruction = Tuple<Axis, number>;
 
 function parseInput(input: string): [Coord[], Instruction[]] {
-  const [dots, instrs] = input.split("\n\n")
+  const [dots, instrs] = input.split("\n\n");
 
   return [
-    lines(dots).map(s => s.split(",").map(Number) as [number, number]),
-    lines(instrs).map(s => {
-      const [str, value] = s.split("=")
+    lines(dots).map((s) => s.split(",").map(Number) as [number, number]),
+    lines(instrs).map((s) => {
+      const [str, value] = s.split("=");
 
-      const axis = str.substring(str.length - 1)
+      const axis = str.substring(str.length - 1);
 
       return [axis, Number(value)] as Instruction;
-    })
+    }),
   ];
 }
--- src/util/tuple.ts
+++ src/util/tuple.ts
@@ -8,6 +8,9 @@ export function second<T>([, snd]: Tuple<any, T>): T {
   return snd;
 }
 
-export function equals<T1, T2>([a1, b1]: Tuple<T1, T2>, [a2, b2]: Tuple<T1, T2>) {
-  return a1 === a2 && b1 === b2
+export function equals<T1, T2>(
+  [a1, b1]: Tuple<T1, T2>,
+  [a2, b2]: Tuple<T1, T2>
+) {
+  return a1 === a2 && b1 === b2;
 }
